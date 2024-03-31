4c6fb0e138ac475c5641e0995d0c25bb9ab208f6
Sari Shehada <sarishehada@gmail.com>
2024-04-01 02:13:06 +0300

    Added start diagnosis button

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:medical_center_patient/config/theme/app_colors.dart';
 import 'package:medical_center_patient/core/extensions/date_time_extensions.dart';
 import 'package:medical_center_patient/core/services/http_service.dart';
-import 'package:medical_center_patient/core/ui_utils/buttons/custom_filled_button.dart';
 import 'package:medical_center_patient/core/ui_utils/spacing_utils.dart';
 import 'package:medical_center_patient/core/widgets/custom_future_builder.dart';
 import 'package:medical_center_patient/managers/account_manager.dart';
-import 'package:medical_center_patient/pages/diagnosis_details_page/diagnosis_details_page.dart';
 import 'package:medical_center_patient/pages/diagnosis_details_page/models/medical_diagnosis_details.dart';
 import 'package:medical_center_patient/pages/home_page/widgets/home_page_top_header_widget.dart';
 import 'package:medical_center_patient/pages/navigation_controller.dart';
@@ -45,12 +44,8 @@ class _HomePageState extends State<HomePage> {
               child: ListView(
                 padding: EdgeInsets.symmetric(horizontal: 10.w),
                 children: [
-                  const UserInfoWidget(),
                   AddVerticalSpacing(value: 10.h),
-                  CustomFilledButton(
-                    onTap: () => NavigationController.startNewDiagnosis(),
-                    child: 'القيام بفحص جديد',
-                  ),
+                  const StartDiagnosisHomePageButton(),
                   AddVerticalSpacing(value: 10.h),
                   Row(
                     children: [
@@ -116,29 +111,67 @@ class MedicalDiagnosisWidget extends StatelessWidget {
   }
 }
 
-class UserInfoWidget extends StatelessWidget {
-  const UserInfoWidget({super.key});
+class StartDiagnosisHomePageButton extends StatelessWidget {
+  const StartDiagnosisHomePageButton({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          'مرحباً ${AccountManager.instance.user!.firstName}',
-          style: TextStyle(
-            fontSize: 24.sp,
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.r),
+        color: secondaryContainer,
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0, 5.h),
+            color: Colors.black.withOpacity(0.08),
+            blurRadius: 8,
           ),
-        ),
-        TitleDetailsSpacedWidget(
-          title: 'تاريخ الميلاد',
-          details: AccountManager.instance.user!.dateOfBirth.getDateOnly(),
-        ),
-        TitleDetailsSpacedWidget(
-          title: 'الجنس',
-          details: AccountManager.instance.user!.isMale ? 'ذكر' : 'انثى',
-        ),
-      ],
+        ],
+      ),
+      child: Stack(
+        children: [
+          Positioned(
+            left: -20.w,
+            // top: -15.h,
+            child: Icon(
+              Icons.monitor_heart,
+              size: 130.sp,
+              color: secondary.withOpacity(0.1),
+            ),
+          ),
+          Material(
+            color: Colors.transparent,
+            child: InkWell(
+              splashColor: Colors.transparent,
+              highlightColor: Colors.white.withOpacity(0.3),
+              onTap: () => NavigationController.startNewDiagnosis(),
+              borderRadius: BorderRadius.circular(10.r),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.h, horizontal: 10.w),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'هل من خطب ما؟',
+                      style: TextStyle(
+                        fontSize: 18.sp,
+                      ),
+                    ),
+                    AddVerticalSpacing(value: 6.h),
+                    Text(
+                      'قم بإجراء فحص طبي ومن ثم متابعة الحالة مع أحد الأطباء لدينا',
+                      style: TextStyle(
+                        fontSize: 22.sp,
+                        color: secondary,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
